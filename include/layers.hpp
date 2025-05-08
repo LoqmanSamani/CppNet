af2db7f9898430249c6c389695000468b48872b9
LoqmanSamani <samaniloqman91@gmail.com>
2025-05-08 23:13:14 +0200

    work ing on two d convolutional layers

--- include/layers.hpp
+++ include/layers.hpp
@@ -2,7 +2,9 @@
 #define LAYERS_HPP
 
 #include <Eigen/Dense>
+#include <unsupported/Eigen/CXX11/Tensor>
 #include <random>
+#include <tuple>
 
 namespace CppNet 
 {
@@ -18,9 +20,14 @@ namespace CppNet
     class Linear : public Layer
     {
         public:
-            bool trainable_; // if gradient should be calculated. if false: layer is frozen.
-
-            Linear(int in_size, int out_size, std::string layer_name = "Linear", bool trainable = true, bool bias = true);
+            
+            Linear(
+                int in_size, 
+                int out_size, 
+                std::string layer_name = "Linear", 
+                bool trainable = true, 
+                bool bias = true
+            );
 
             Eigen::MatrixXd forward(const Eigen::MatrixXd& X);
 
@@ -43,6 +50,8 @@ namespace CppNet
             void set_biases(const Eigen::VectorXd& biases) { biases_ = biases; }
             std::string get_layer_name() const { return layer_name_; }
             bool is_trainable() const override { return trainable_; }
+            void freeze(){ trainable_ = false; } // freeze the parameters of the layer.
+            void unfreeze() { trainable_ = true; } // unfreeze the parameters of the layer.
             bool has_bias() const { return bias_; }
             void update_parameters(Optimizer& optimizer, double learning_rate) override;
 
@@ -56,8 +65,84 @@ namespace CppNet
             Eigen::MatrixXd in_cache_;
             Eigen::MatrixXd grad_weights_;
             Eigen::VectorXd grad_biases_;
+            bool trainable_; // if gradient should be calculated. if false: layer is frozen.
 
             void init_params_and_grads();
+    };
+
+    class Conv2d: public Layer
+    {
+        public:
+
+            Conv2d(
+                int in_channels,
+                int out_channels,
+                std::tuple<int, int> kernel_size = std::make_tuple(3, 3),
+                std::tuple<int, int> stride = std::make_tuple(1, 1), 
+                std::string padding = "valid", 
+                std::tuple<int, int> num_padding = std::make_tuple(2, 2), 
+                std::string padding_mode = "zero", 
+                std::string layer_name = "Conv2D", 
+                bool trainable = true, 
+                bool bias = true
+            );
+
+            Eigen::Tensor<double, 4> forward(Eigen::Tensor<double, 4>& X);
+            Eigen::Tensor<double, 4> backward(const Eigen::Tensor<double, 4>& grad_out);
+
+            void reset_grads() {
+                grad_weights_.setZero();
+                if (bias_) {
+                    grad_biases_.setZero();
+                }
+            }
+
+            Eigen::Tensor<double, 4>& get_weights() { return weights_; }
+            const Eigen::Tensor<double, 4>& get_weights() const { return weights_; }
+            Eigen::VectorXd& get_biases() { return biases_; }
+            const Eigen::VectorXd& get_biases() const { return biases_; }
+            const Eigen::Tensor<double, 4>& get_grad_weights() const { return grad_weights_; }
+            const Eigen::VectorXd& get_grad_biases() const { return grad_biases_; }
+            void set_weights(const Eigen::Tensor<double, 4>& weights) { weights_ = weights; }
+            void set_biases(const Eigen::VectorXd& biases) { biases_ = biases; }
+            std::string get_layer_name() const { return layer_name_; }
+            bool is_trainable() const override { return trainable_; }
+            void freeze(){ trainable_ = false; } // freeze the parameters of the layer.
+            void unfreeze() { trainable_ = true; } // unfreeze the parameters of the layer.
+            bool has_bias() const { return bias_; }
+            void update_parameters(Optimizer& optimizer, double learning_rate) override;
+
+        private:
+
+            int in_channels_;
+            int out_channels_;
+            bool trainable_;
+            std::tuple<int, int> stride_;
+            std::string padding_;
+            std::tuple<int, int, int, int> num_padding_; // if padding is set to "none", the amount of padding from this tuple will be used to padding (left, right, top bottom)
+            std::string padding_mode_;
+            bool bias_;
+            Eigen::Tensor<double, 4> in_cache_;
+            std::tuple<int, int> kernel_size_;
+            std::string layer_name_;
+            Eigen::Tensor<double, 4> weights_;
+            Eigen::VectorXd biases_;
+            Eigen::Tensor<double, 4> in_cache_;
+            Eigen::Tensor<double, 4> output_;
+            Eigen::Tensor<double, 4> grad_weights_;
+            Eigen::VectorXd grad_biases_;
+
+            // input shape
+            int B_; // batch size
+            int C_; // number of channels
+            int H_; // heigth of data (e.g., images)
+            int W_; // width of data (e.g., images)
+            int h_; // heigth of output data
+            int w_;
+            
+            void init_params_and_grads();
+            void init_output();
+            Eigen::Tensor<double, 4> pad_input(Eigen::Tensor<double, 4>& X);
     };
 }
 
--- src/layers.cpp
+++ src/layers.cpp
@@ -102,4 +102,120 @@ namespace CppNet
         return grad_in;
     }
 
+    Conv2d::Conv2d(
+        int in_channels, int out_channels, std::tuple<int, int> kernel_size, std::tuple<int, int> stride, std::string padding,
+        std::tuple<int, int> num_padding, std::string padding_mode, std::string layer_name, bool trainable, bool bias)
+        : in_channels_(in_channels), out_channels_(out_channels), kernel_size_(kernel_size), stride_(stride),
+        padding_(padding), num_padding_(num_padding), padding_mode_(padding_mode), layer_name_(layer_name),
+        trainable_(trainable), bias_(bias)
+        {
+            // check if in and out sizes are positive integers
+            if (in_channels <= 0 || out_channels <= 0)
+            {
+                throw std::runtime_error("in_chnnels and out_channels of layer: " + layer_name + " must be positive integers!");
+            }
+
+            // initialize filters, biases and gradients 
+            init_params_and_grads();   
+        }
+
+    void Conv2d::init_output()
+    {
+
+        // TODO: methods for "valid" & "same" should be implemented.
+
+        // compute output shape if padding_is none (not valid or same)
+        if (padding_ == "none")
+        {
+            double h_f = (H_ + 2.0 * std::get<0>(num_padding_) - static_cast<double>(std::get<0>(kernel_size_)) + 1.0) / static_cast<double>(std::get<0>(stride_));
+            double w_f = (W_ + 2.0 * std::get<1>(num_padding_) - static_cast<double>(std::get<1>(kernel_size_)) + 1.0) / static_cast<double>(std::get<1>(stride_));
+
+            h_ = static_cast<int>(std::floor(h_f));
+            w_ = static_cast<int>(std::floor(w_f));
+        }
+        // initialize output tensor with zeros and computed shape
+        output_.resize(B_, C_, h_, w_);
+        output_.setZero();
+
+    }
+
+    Eigen::Tensor<double, 4> Conv2d::forward(Eigen::Tensor<double, 4>& X)
+    {
+        // X shape(batch, channels, heigth, width)
+        B_ = X.dimension(0); // batch size
+        C_ = X.dimension(1); // number of channels
+        H_ = X.dimension(2) + std::get<2>(num_padding_) + std::get<3>(num_padding_); // heigth of data (e.g., images)
+        W_ = X.dimension(3) + std::get<0>(num_padding_) + std::get<0>(num_padding_); // width of data (e.g., images)
+
+        Eigen::Tensor<double, 4> pad_x = pad_input(X); 
+
+        init_output(); // initialize output tensor
+    }
+
+
+
+
+    void Conv2d::init_params_and_grads()
+    {
+        weights_.resize(out_channels_, in_channels_, std::get<0>(kernel_size_), std::get<1>(kernel_size_));
+        grad_weights_.resize(out_channels_, in_channels_, std::get<0>(kernel_size_), std::get<1>(kernel_size_));
+        
+        std::random_device rd;
+        std::mt19937 gen(rd());
+
+        double scale = std::sqrt(6.0 / (in_channels_ + out_channels_));
+        std::uniform_real_distribution<> dis(-scale, scale);
+
+        // initializing weights with Xavier method (default method).
+        weights_.generate([&]() { return dis(gen); });
+        // initialize weight-gradient matrix with zero
+        grad_weights_.setZero();
+
+        if (bias_)
+        {
+            // initialize biases with zero, if bias_ is true.
+            biases_ = Eigen::VectorXd::Zero(out_channels_); 
+            // initialize bias-gradient matrix with zero 
+            grad_biases_ = Eigen::VectorXd::Zero(out_channels_);
+        }
+        else
+        {
+            // initialize empty biases and gradients when bias is false
+            biases_ = Eigen::VectorXd(0);
+            grad_biases_ = Eigen::VectorXd(0);
+        }
+    }
+
+    Eigen::Tensor<double, 4> Conv2d::pad_input(Eigen::Tensor<double, 4>& X)
+    {
+        Eigen::Tensor<double, 4> pad_x(B_, C_, H_, W_);
+        pad_x.setZero();
+
+        // TODO: implement other methods
+        if (padding_mode_ == "zero")
+        {
+            
+            for (int i = 0; i < B_; i++)
+            {
+                for (int j = 0; j < C_; j++)
+                {
+                    for (int h = std::get<2>(num_padding_); h < X.dimension(2) - std::get<2>(num_padding_) - std::get<3>(num_padding_); h++)
+                    {
+                        for (int w = std::get<0>(num_padding_); w < X.dimension(3) - std::get<0>(num_padding_) - std::get<1>(num_padding_); w++)
+                        {
+                            pad_x(i, j, h, w) = X(i, j, h, w);
+                        }
+                    }
+                }
+            }
+
+        }
+        return pad_x;
+    }
+
+    Eigen::Tensor<double, 4> Conv2d::backward(const Eigen::Tensor<double, 4>& grad_out)
+    {
+
+    }
+
 }
